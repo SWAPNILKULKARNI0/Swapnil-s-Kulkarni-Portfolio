69400d9cfa8a5986f27899e31b8824c00714fa7b
SWAPNIL KULKARNI <109028027+SWAPNILKULKARNI0@users.noreply.github.com>
2023-09-25 03:43:29 +0530

    Inital version  0.1V
    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        
+    };
+
+    const serviceID = "service_sucn58f";
+    const templateID = "template_e1qm255";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            res => {
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("message").value = "";
+                
+                console.log(res);
+                alert("Your Message Sent Successfully!!!!!")
+
+            }
+        )
+
+        .catch((err) => console.log(err));
+}
+
